bf0c31c56013280cde31fd202053e8a67116fef1
arunrnair65 <arunrn.65@gmail.com>
2022-03-16 23:29:06 +1100

    added Colors

--- print.sh
+++ print.sh
@@ -1,1 +1,18 @@
-echo "Arun"
+echo arun
+echo -e This is "\e[31mRED\e[0m" color
+
+#red   31
+#green 32
+#yellow 33
+#blue   34
+#magenta 35
+#cyan 36
+#syntax  : echo -e "\e[31mHello\e[0m"
+# -e option to enable escape chars
+# "" quotes are mandatory for colors to work, we can use singe quotes as well
+# \e[COLm  -> To enable color, COL is colorcode
+#\e[0m --> This is to disable the cole from continuing (stop color)
+
+
+
+
