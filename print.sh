42c113a03b3481fbcd1bf336262f2e94589eec2a
arunnair <arunrn.65@gmail.com>
2022-03-20 22:18:29 +1100

    Add todays date

--- print.sh
+++ print.sh
@@ -17,5 +17,5 @@ echo -e "Line1\nLine2"   #\n for new line
 echo -e "Tab1\tTab2"   #\t for Tab
 
 
-tdate = $(date +%F)
-echo -e "todays date is "\e[32m$tdate\e[0m""
+tdate = (date +%F)
+echo -e "todays date is \e[32m$tdate\e[0m
