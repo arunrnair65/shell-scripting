00b1590ddaf3e510f6820a391a79ea8d407e92a6
arunrnair65 <arunrn.65@gmail.com>
2022-04-03 11:21:58 +1000

    This is to set environment Variables

--- input.sh
+++ input.sh
@@ -1,6 +1,16 @@
 #get inputs
 
+#-p is to print the message
 read -p "enter your name : " name
 echo "Name is $name"
 
 
+#special variables
+#($0 to $n), $* or $@, $#
+
+echo "the script name is $0"
+echo " the first argument is $1"
+echo "the secong argument is $2"
+echo "the third argument is $3"
+echo "all arguments are $*"
+echo "total number of arugument is $#"
