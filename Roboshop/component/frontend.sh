920fe462b6ecd9e0d37b9511ba541287f7468968
arunrnair65 <arunrn.65@gmail.com>
2022-04-25 15:20:06 +1000

    Added user id checks added exit status

--- Roboshop/component/frontend.sh
+++ Roboshop/component/frontend.sh
@@ -8,9 +8,23 @@ fi
 
 echo -e "\e[32m Installing Nginx... \e[0m"
 yum install nginx -y
+#add error checks
+if [ $? -eq 0] ; then
+  echo -e "\e[32Successfully installed nginx..\e[0m"
+else
+  echo -e "\e[31mFailed to install nginx...\e[0m"
+  exit 2
+fi
 
 echo -e "\e[32m Downloading WebContent \e[0m"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+#add error checks
+if [ $? -eq 0] ; then
+  echo -e "\e[32Successfully downloaded content..\e[0m"
+else
+  echo -e "\e[31mFailed to download content...\e[0m"
+  exit 2
+fi
 
 echo -e "\e[32m Cleanup new content and extract new content... \e[0m"
 rm -rf /usr/share/nginx/html/*
@@ -26,4 +40,10 @@ systemctl enable nginx
 systemctl start nginx
 systemctl restart nginx
 
+if [ $? -eq 0] ; then
+  echo -e "\e[32Successfully install rob webserver..\e[0m"
+else
+  echo -e "\e[31mFailed install rob webserver...\e[0m"
+  exit 2
+fi
 
