ec64d8306add3db2a80bec0f51b6040660d841bf
arunrnair65 <arunrn.65@gmail.com>
2022-04-25 12:35:15 +1000

    If Condition

--- condition.sh
+++ condition.sh
@@ -3,10 +3,38 @@
 #if condition
 #if [expression]
 # then
-#commands
+#...commands...
 #fi
 
 if [ 1 -eq 1 ]
 then
   echo "matched.."
 fi
+
+#expressions
+#1. String Tests
+#OPerators : ==, !=, -z,
+
+a= "abc"
+if [ $a == "ABC" ]
+then
+  echo "both are not equal"
+fi
+
+if [$a != "abc"]; then
+  echo "both are not equal"
+fi
+
+if [ -z $b]; then
+  echo "$b is empty"
+fi
+
+
+
+
+
+
+
+
+
+
